7c63f8dd9ae4e7435e5a035cc3515bae290b7235
Lance Johnson <rlancejohnson@gmail.com>
2022-01-10 21:38:36 -0700

    Create new tweet cmp with input and submit button

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,20 @@ import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
 import Dashboard from './Dashboard';
 import LoadingBar from 'react-redux-loading';
+import NewTweet from './NewTweet';
 
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
+
   render() {
     return (
       <div>
         <LoadingBar />
         {this.props.loading === true
           ? null
-          : <Dashboard />}
+          : <NewTweet />}
       </div>
     )
   }
